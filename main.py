7e896dc1ca9bd339025ecee7e934fedb97079de4
Hiroaki Abe <hiroaki0404@gmail.com>
2022-12-28 03:59:53 +0900

    Stringfy. see https://tech.blog.home.group.jp/lerning-python/python-stringfy

--- main.py
+++ main.py
@@ -1,76 +1,40 @@
 #!/usr/bin/env python
 #
 # Hit and Blow
-#   慣れていない人が書くとこんな感じ？
-#   型ヒント追加
+#   文字列で扱えばx桁目の数字を簡単に扱える
 #   
 import random
 
-def is_number_correct(target: int) -> bool:
-    k1: int = target % 10
-    k2: int = (target // 10) % 10
-    k3: int = (target // 100) % 10
-    k4: int = target // 1000
-    if k2 == k1 or \
-        k3 == k2 or k3 == k1 or \
-        k4 == k3 or k4 == k2 or k4 == k1:
+def is_number_correct(target: str) -> bool:
+    if target[1] == target[0] or \
+        target[2] == target[1] or target[2] == target[0] or \
+        target[3] == target[2] or target[3] == target[1] or target[3] == target[0]:
         return False
     return True
 
-def target_num() -> int:
+def target_num() -> str:
     while True:
-        t: int = random.randint(123,9876)
+        t: str = str(random.randint(123,9876)).zfill(4)
         if is_number_correct(t):
-            return t
+            return str(t)
 
-def count_hit(com_num: int, player_num: int) -> int:
+def count_hit(com_num: str, player_num: str) -> int:
     count: int = 0
-    if com_num % 10 == player_num % 10:
-        count += 1
-    if (com_num // 10) % 10 == (player_num // 10) % 10:
-        count += 1
-    if (com_num // 100) % 10 == (player_num // 100) % 10:
-        count += 1
-    if com_num // 1000 == player_num // 1000:
-        count += 1
+
+    for i in range(4):
+        if com_num[i] == player_num[i]:
+            count += 1
     return count
 
-def count_blow(com_num: int, player_num: int, hit_num: int) -> int:
+def count_blow(com_num: str, player_num: str, hit_num: int) -> int:
     count: int = 0
 
-    com_keta_1: int = com_num % 10
-    com_keta_2: int = (com_num // 10) % 10
-    com_keta_3: int = (com_num // 100) % 10
-    com_keta_4: int = com_num // 1000
-
-    player_keta_1: int = player_num % 10
-    player_keta_2: int = (player_num // 10) % 10
-    player_keta_3: int = (player_num // 100) % 10
-    player_keta_4: int = player_num // 1000
-
-    if com_keta_1 == player_keta_1 or \
-        com_keta_1 == player_keta_2 or \
-        com_keta_1 == player_keta_3 or \
-        com_keta_1 == player_keta_4:
-        count += 1
-    if com_keta_2 == player_keta_1 or \
-        com_keta_2 == player_keta_2 or \
-        com_keta_2 == player_keta_3 or \
-        com_keta_2 == player_keta_4:
-        count += 1
-    if com_keta_3 == player_keta_1 or \
-        com_keta_3 == player_keta_2 or \
-        com_keta_3 == player_keta_3 or \
-        com_keta_3 == player_keta_4:
-        count += 1
-    if com_keta_4 == player_keta_1 or \
-        com_keta_4 == player_keta_2 or \
-        com_keta_4 == player_keta_3 or \
-        com_keta_4 == player_keta_4:
-        count += 1
+    for i in range(4):
+        if com_num[i] in player_num:
+            count += 1
     return count - hit_num
 
-def judge(com_num: int, player_num: int) -> bool:
+def judge(com_num: str, player_num: str) -> bool:
     if com_num == player_num:
         print("Congraturation!")
         return True
@@ -80,9 +44,14 @@ def judge(com_num: int, player_num: int) -> bool:
     print(f'{hit_num} hit {blow_num} blow')
     return False
 
+def input_number() -> str:
+    p: str = "0000" + input()
+    l = len(p)
+    return p[l-4:]
+
 def main() -> None:
-    t: int = target_num()
-    while not judge(t, int(input())):
+    t: str = target_num()
+    while not judge(t, input_number()):
         pass
     return
 
