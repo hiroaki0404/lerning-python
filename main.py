8868211034a0a412dfaa48938d29d4db00d726b7
Hiroaki Abe <hiroaki0404@gmailcom>
2023-01-10 12:56:04 +0000

    Web (Flask)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 #
 # Hit and Blow
-#   文字列で扱えばx桁目の数字を簡単に扱える
+#   Web version
 #   
 import random
+from flask import Flask, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 def is_number_correct(target: str) -> bool:
     if target[1] == target[0] or \
@@ -34,26 +39,31 @@ def count_blow(com_num: str, player_num: str, hit_num: int) -> int:
             count += 1
     return count - hit_num
 
-def judge(com_num: str, player_num: str) -> bool:
+def judge(com_num: str, player_num: str) -> tuple[str, int, int]:
     if com_num == player_num:
-        print("Congraturation!")
-        return True
+        return com_num, 4, 0
     hit_num = count_hit(com_num, player_num)
     blow_num = count_blow(com_num, player_num, hit_num)
 
-    print(f'{hit_num} hit {blow_num} blow')
-    return False
+    return player_num, hit_num, blow_num
 
-def input_number() -> str:
-    p: str = "0000" + input()
-    l = len(p)
-    return p[l-4:]
+@app.route("/", methods = ["GET"])
+def main():
+    session["target"] = target_num()
+    session["history"] = []
+    return render_template("index.html")
 
-def main() -> None:
-    t: str = target_num()
-    while not judge(t, input_number()):
-        pass
-    return
+@app.route("/", methods=["POST"])
+def play():
+    t: str = "0000" + (request.form.get("number") or "" )
+    p: str = t[len(t)-4:]
+    j = judge(session["target"], p)
+    h =session["history"]
+    h.append(j)
+    session["history"] = h
+    if j[1] == 4:
+        return render_template("win.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
